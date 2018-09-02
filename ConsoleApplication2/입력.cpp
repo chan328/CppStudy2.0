94af27ba9522fb2d60ffcb28c9b44fcfa6f42b72
chan <rkdcks020328@gmail.com>
2018-09-02 22:27:46 +0900

    first

---
+++ ConsoleApplication2/입력.cpp
@@ -0,0 +1,13 @@
+#include "pch.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string name;
+	cout << "이름을 입력하고 엔터를 눌러주세요\n";
+	cin >> name;
+	cout << "안녕하세요, " << name;
+}
---
+++ ConsoleApplication2/출력.cpp
@@ -0,0 +1,10 @@
+#include "pch.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	std::cout << "ÃÖÁö¿õ, ¾È³ç!\n"; // ÃÖÁö¿õ, ¾È³ç! Ãâ·Â
+	std::cout << "Àß°¡!"
+	return 0;
+}
